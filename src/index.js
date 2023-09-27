64f809fce60d1fe16596a3c6e0c5bc594d7df24a
danie_h <a01423393@tec.mx>
2023-09-27 11:07:10 -0600

    bienvenida

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import multer from "multer";
 import info_estado from "./route/info_estado.js"
 import proyecto from './route/proyecto.js'
+import bienvenida from './route/bienvenida.js'
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,8 +12,10 @@ app.use(cors());
 app.use(multer().array());
 app.use(express.json());
 
+app.use('/',bienvenida); //get
 app.use('/info_estado',info_estado); //get
 app.use('/proyecto',proyecto); //get | put | post | delete
+//app.use('/ubicar'); //get
 
 app.listen (port, () => {
     console.log(`servidor iniciado en el puerto ${port}`)
